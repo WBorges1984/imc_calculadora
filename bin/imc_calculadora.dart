e7df33411cb1e4241fb2f2aec3da8935038080fd
Willian.Borges <willianodhara@gmail.com>
2023-10-31 09:15:31 -0300

    Validações

--- bin/imc_calculadora.dart
+++ bin/imc_calculadora.dart
@@ -1,36 +1,17 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:imc_calculadora/Exception/Erro_Altura_Null.dart';
-import 'package:imc_calculadora/Exception/erro_peso_null.dart';
-import 'package:imc_calculadora/classes/pessoa.dart';
-import 'package:imc_calculadora/functions/calculo_imc.dart';
-
 void main() {
-  print("Digite o nome da pessoa ");
-  var lineNome = stdin.readLineSync(encoding: utf8);
-  var nome = lineNome ?? "";
+  double valor = double.parse(stdin.readLineSync()!);
 
-  print("Digite o peso da pessoa ");
-  var linePeso = stdin.readLineSync(encoding: utf8);
-  var peso = double.tryParse(linePeso!);
-  if (peso == null) {
-    peso = ErroPesoNull().ps;
-    throw ErroPesoNull();
+  while (valor < 0) {
+    print("Valor inválido! Digite um valor maior que zero.");
+    valor = double.parse(stdin.readLineSync()!);
   }
 
-  print("Digite a altura da pessoa ");
-  var lineAltura = stdin.readLineSync(encoding: utf8);
-  var altura = double.tryParse(lineAltura!);
-  if (altura == null) {
-    throw ErroAlturaNull();
+  if (valor > 0) {
+    print("Depósito realizado com sucesso!");
+    print("Saldo atual: R\$ 500.50");
+  } else if (valor == 0) {
+    print("Encerrando o programa...");
   }
-  Pessoa pessoa1 = Pessoa(nome, peso, altura);
-
-  double imc = calcularImc(pessoa1);
-  String resposta = obterRespostaIMC(imc);
-
-  print('Pessoa: ${pessoa1.getNome()}');
-  print('Tem o IMC de : $imc');
-  print('Esta com a saude de  : $resposta');
 }
